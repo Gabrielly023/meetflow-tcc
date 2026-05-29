f34d878ee8280c37cb4a161e77daf4ca5d8bd26a
mariab <mariab>
2026-05-29 11:57:26 -0300

    tailwind arrumado

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -4,7 +4,7 @@ const Header = ({ title = 'MeetFlow', links = [{ label: 'Home', href: '/' }, { l
 	const [open, setOpen] = useState(false)
 
 	return (
-	<header className="text-gray-400 bg-red-500 body-font">
+	<header className="text-gray-400 bg-red-900 body-font">
   <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
     <a className="flex title-font font-medium items-center text-white mb-4 md:mb-0">
       <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-10 h-10 text-white p-2 bg-indigo-500 rounded-full" viewBox="0 0 24 24">
