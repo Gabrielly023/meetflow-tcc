3d87bd1ebc4cee4e78e8f8e524e29c862d4f0e97
hfcosta <hfcosta>
2026-08-20 11:18:39 -0300

    removendo migrations antigas

--- backend/prisma/migrations/20260814181231_add_perfil_campos_usuario/migration.sql
+++
@@ -1,5 +0,0 @@
--- AlterTable
-ALTER TABLE `usuario` ADD COLUMN `bio` TEXT NULL,
-    ADD COLUMN `foto_capa` VARCHAR(500) NULL,
-    ADD COLUMN `localizacao` VARCHAR(150) NULL,
-    ADD COLUMN `site` VARCHAR(255) NULL;
--- backend/prisma/migrations/20260814182309_add_grupo_campos_evento/migration.sql
+++
@@ -1,5 +0,0 @@
--- AlterTable
-ALTER TABLE `evento` ADD COLUMN `descricao_grupo` TEXT NULL,
-    ADD COLUMN `foto_grupo` VARCHAR(500) NULL,
-    ADD COLUMN `nome_grupo` VARCHAR(150) NULL,
-    ADD COLUMN `papel_parede` VARCHAR(500) NULL;
